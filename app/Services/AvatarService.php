43405461cc10e959ca0fa9475066336d58b262a5
Don Herre <donherre@gmail.com>
2016-03-24 12:24:59 -0400

    Added automatic saving of 100x100 and 250x250 photo copies when adding a new avatar.

--- app/Services/AvatarService.php
+++ app/Services/AvatarService.php
@@ -33,18 +33,44 @@ class AvatarService {
 
         /* find extension and name image file */
         $ext = AvatarService::getExtFromMimeType($img->mime());
-        $f_name = 'UID' . $user->id . '_' . time() . '.' . $ext;
+        $f_name = 'UID' . $user->id . '_' . time();
 
         /* gather image details */
         $details = [
             'extension' => $ext,
             'orig_name' => $orig_name,
-            'path' => 'avatar/' . $f_name,
+            'path' => 'avatar/' . $f_name . '.' . $ext,
+            'path_sm' => 'avatar/' . $f_name . '_sm.' . $ext,
+            'path_md' => 'avatar/' . $f_name . '_md.' . $ext,
         ];
 
         /* store file on server */
         $path = public_path($details['path']);
         $img->save($path);
+
+        /* create instances for small and medium images */
+        $img_md = Image::make($path);
+        $img_sm = Image::make($path);
+
+        /* resize images for thumbnails preventing upscaling */
+        $img_md->resize(250, 250, function ($constraint) {
+            $constraint->aspectRatio();
+            $constraint->upsize();
+        });
+
+        $img_sm->resize(100, 100, function ($constraint) {
+            $constraint->aspectRatio();
+            $constraint->upsize();
+        });
+
+        /* crop thumbnails to ensure proper sizing */
+        $img_md->crop(250, 250);
+        $img_sm->crop(100, 100);
+
+        /* save edited images to server */
+        $img_md->save(public_path($details['path_md']));
+        $img_sm->save(public_path($details['path_sm']));
+
 
         /* create entry */
         AvatarService::createAvatars($user, $img, $details);
@@ -60,6 +86,8 @@ class AvatarService {
         UserAvatar::create([
             'user_id' => $user->id,
             'file_path' => $details['path'],
+            'file_path_sm' => $details['path_sm'],
+            'file_path_md' => $details['path_md'],
             'original_name' => $details['orig_name'],
             'extension' => $details['extension'],
             'size' => $img->filesize(),
